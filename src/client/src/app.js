99df8ee3bdcc56aba0562027e488c599fcd642fd
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-11-24 19:05:47 -0600

    simplify things

--- src/client/src/app.js
+++ src/client/src/app.js
@@ -1,12 +1,12 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Router} from './components/router';
 import {NavigationLoadable} from './loadables/navigation';
 
 export const App = () =>
     <BrowserRouter>
-        <Fragment>
+        <>
             <NavigationLoadable/>
             <Router/>
-        </Fragment>
+        </>
     </BrowserRouter>;
--- src/client/src/components/page.js
+++ src/client/src/components/page.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
-import React, {Fragment} from 'react';
+import React from 'react';
 import DocumentTitle from 'react-document-title';
 
 const getTitle = props => `Expense Manager${props.title ? ` | ${props.title}` : ''}`;
 
 export const Page = props =>
     <DocumentTitle title={getTitle(props)}>
-        <Fragment>
+        <>
             <h2>{props.title}</h2>
             {props.children}
-        </Fragment>
+        </>
     </DocumentTitle>;
 
 Page.displayName = 'Page';
