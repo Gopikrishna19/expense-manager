12c82d633de6751277ff0264c95195c3b70f814d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-12-30 13:06:15 -0600

    make the charts interactive

--- src/client/src/components/summary-chart.js
+++ src/client/src/components/summary-chart.js
@@ -1,88 +1,38 @@
-import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import {array, bool, func} from 'prop-types';
+import React from 'react';
 import {Chart} from 'react-google-charts';
-import {entryCategories, entryFields} from '../../../schema/state/entry';
-import {query} from '../utils/gql';
 
-const getChartEvents = () => [
-    {
-        callback({chartWrapper}) {
-            chartWrapper.getChart().getSelection();
-        },
-        eventName: 'select'
-    }
-];
+export const SummaryChart = props =>
+    <>
+        {
+            props.isDetailed ?
+                <Button variant='text' onClick={props.onDetailedViewExit}><ArrowBack/></Button> :
+                <Button variant='text' disabled><SvgIcon><span/></SvgIcon></Button>
+        }
+        <Chart
+            height='500px'
+            chartEvents={props.chartEvents}
+            chartType='PieChart'
+            data={[
+                ['Reason', 'Amount'],
+                ...props.renderData
+            ]}
+            options={{
+                animation: {
+                    duration: 1000,
+                    easing: 'out',
+                    startup: true
+                }
+            }}
+        />
+    </>;
 
-class StateManager {
-    constructor(query) {
-        this.query = query;
-        this.rawData = [];
-    }
-
-    static aggregateData(items, field) {
-        return Object.entries(
-            items.reduce(
-                (group, debit) =>
-                    Object.assign(group, {[debit[field]]: (group[debit[field]] || 0) + debit.amount}),
-                {}
-            )
-        );
-    }
-
-    async initialize(getData) {
-        this.rawData = await this.query().then(getData);
-
-        return this.rawData;
-    }
-
-    getRenderData() {
-        this.cumulativeData = StateManager.aggregateData(this.rawData, entryFields.category);
-
-        return this.cumulativeData.map(([key, value]) => [entryCategories[key], value]);
-    }
-}
-
-export class SummaryChart extends Component {
-    debitManager = new StateManager(query`debits {
-        amount
-        category
-        reason
-    }`);
-    creditManager = new StateManager(query`credits {
-        amount
-        category
-        reason
-    }`);
-
-    componentDidMount() {
-        Promise.all([
-            this.debitManager.initialize(({debits}) => debits),
-            this.creditManager.initialize(({credits}) => credits)
-        ])
-            .then(() => this.setState({}));
-    }
-
-    render() {
-        return (
-            <>
-                <Chart
-                    height='500px'
-                    chartEvents={getChartEvents(this, this.debitManager)}
-                    chartType='PieChart'
-                    data={[
-                        ['Reason', 'Amount'],
-                        ...this.debitManager.getRenderData()
-                    ]}
-                />
-                <Chart
-                    height='500px'
-                    chartEvents={getChartEvents(this, this.creditManager)}
-                    chartType='PieChart'
-                    data={[
-                        ['Reason', 'Amount'],
-                        ...this.creditManager.getRenderData()
-                    ]}
-                />
-            </>
-        );
-    }
-}
+SummaryChart.propTypes = {
+    chartEvents: array.isRequired,
+    isDetailed: bool.isRequired,
+    onDetailedViewExit: func.isRequired,
+    renderData: array.isRequired
+};
--- src/client/src/components/summary.js
+++ src/client/src/components/summary.js
@@ -1,8 +1,133 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {entryCategories, entryFields} from '../../../schema/state/entry';
+import {query} from '../utils/gql';
 import {Page} from './page';
 import {SummaryChart} from './summary-chart';
 
-export const Summary = () =>
-    <Page title='Summary'>
-        <SummaryChart/>
-    </Page>;
+const setAggregateView = (component, manager) => () => {
+    if (manager.isDetailed) {
+        manager.setAggregateRenderData();
+        component.setState({});
+    }
+};
+
+const setDetailedView = (component, manager) => (chart) => {
+    if (!manager.isDetailed) {
+        const category = manager.categories[chart.getSelection()[0].row];
+
+        manager.setDetailedRenderData(category);
+        component.setState({});
+    }
+};
+
+const getChartEvents = (component, manager) => {
+    const detailedViewHandler = setDetailedView(component, manager);
+
+    return [
+        {
+            callback: ({chartWrapper}) => detailedViewHandler(chartWrapper.getChart()),
+            eventName: 'select'
+        }
+    ];
+};
+
+class StateManager {
+    #categories = [];
+    #query = '';
+    #rawData = [];
+    #renderData = [];
+    #detailed = false;
+
+    constructor(query) {
+        this.#query = query;
+    }
+
+    static aggregateData(items, field) {
+        return Object.entries(
+            items.reduce(
+                (group, debit) =>
+                    Object.assign(group, {[debit[field]]: (group[debit[field]] || 0) + debit.amount}),
+                {}
+            )
+        );
+    }
+
+    get categories() {
+        return this.#categories;
+    }
+
+    get isDetailed() {
+        return this.#detailed;
+    }
+
+    get renderData() {
+        return this.#renderData;
+    }
+
+    setAggregateRenderData() {
+        const cumulativeData = StateManager.aggregateData(this.#rawData, entryFields.category);
+
+        this.#detailed = false;
+        this.#categories = cumulativeData.map(([key]) => key);
+        this.#renderData = cumulativeData.map(([key, value]) => [entryCategories[key], value]);
+    }
+
+    setDetailedRenderData(category) {
+        this.#renderData = StateManager.aggregateData(
+            this.#rawData
+                .filter(entry => entry.category === category)
+                .map(entry => ({
+                    ...entry,
+                    reason: entry.reason.split('/').pop()
+                })),
+            entryFields.reason
+        );
+        this.#detailed = true;
+    }
+
+    async initialize(getData) {
+        this.#rawData = await this.#query().then(getData);
+
+        this.setAggregateRenderData();
+    }
+}
+
+export class Summary extends Component {
+    debitManager = new StateManager(query`debits {
+        amount
+        category
+        reason
+    }`);
+    creditManager = new StateManager(query`credits {
+        amount
+        category
+        reason
+    }`);
+
+    componentDidMount() {
+        Promise.all([
+            this.debitManager.initialize(({debits}) => debits),
+            this.creditManager.initialize(({credits}) => credits)
+        ])
+            .then(() => this.setState({}));
+    }
+
+    render() {
+        return (
+            <Page title='Summary'>
+                <SummaryChart
+                    isDetailed={this.debitManager.isDetailed}
+                    onDetailedViewExit={setAggregateView(this, this.debitManager)}
+                    chartEvents={getChartEvents(this, this.debitManager)}
+                    renderData={this.debitManager.renderData}
+                />
+                <SummaryChart
+                    isDetailed={this.creditManager.isDetailed}
+                    onDetailedViewExit={setAggregateView(this, this.creditManager)}
+                    chartEvents={getChartEvents(this, this.creditManager)}
+                    renderData={this.creditManager.renderData}
+                />
+            </Page>
+        );
+    }
+}
